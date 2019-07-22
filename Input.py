ca8c2de7213d19d64e67623eb4b22aeb0fe00688
ARMANDArthur <armand.arthur@gmail.com>
2019-07-22 16:56:15 +0200

    algorithme de substituion améliorer, color et retour à la ligne dans Input, verificatio de la saisie

--- Input.py
+++ Input.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 from Database import Database
-
+from Color import Color
 class Input:
 
     def __init__(self):
@@ -10,19 +10,22 @@ class Input:
         self.display_menu()
 
     def display_menu(self):
-        print(" 1 - Quel aliment souhaitez-vous remplacer ? ")
-        print(" 2 - Retrouver mes aliments substitues ")
-        choice_menu = input("Faites votre choix : ")
-        choice_menu = int(choice_menu)
+        print("\n")
+        print(Color.WARNING + " 1 - Quel aliment souhaitez-vous remplacer ? " + Color.ENDC)
+        print(Color.WARNING + " 2 - Retrouver mes aliments substitues " + Color.ENDC)
+        print("\n")
+        choice_menu = input(Color.HEADER + "Faites votre choix : " + Color.ENDC)
+        try:
+            choice_menu = int(choice_menu)
+        except ValueError:
+            self.display_menu()
+        
 
         if choice_menu == 1 :
             self.categorie_menu()
         # Retrouver mes substitus
         elif choice_menu == 2 :
-        # Autrement, on relance la question primaire
             print('ko')
-        else :
-            self.display_menu()
 
     def categorie_menu(self):
         # pour avoir les attributs au lieu des tuples
@@ -30,18 +33,25 @@ class Input:
         sql = "SELECT * FROM categories"
         cursor.execute(sql)
         categories = cursor.fetchall()
-
+        print("\n")
+        print(Color.OKGREEN + "Le programme a retourné {} catégorie".format(cursor.rowcount)+ Color.ENDC)
+        print("\n")
         self.categorie_print(categories)
         self.categorie_input()
 
     def categorie_print(self, categories):
         for categorie in categories:
-            print ("{} -- {}".format(categorie.id,categorie.name))
+            print (Color.WARNING + "{} -- {}".format(categorie.id, categorie.name)+ Color.ENDC)
+        print("\n")
     
     def categorie_input(self):
-        choice_categorie = input("Selectionner l'id d'une categorie : ")
-        choice_categorie = int(choice_categorie)
-        self.categorie_exist(choice_categorie)
+        choice_categorie = input(Color.HEADER + "Selectionner l'id d'une categorie : "+ Color.ENDC)
+        try:
+            choice_categorie = int(choice_categorie)
+            self.categorie_exist(choice_categorie)
+        except ValueError:
+            self.categorie_menu()        
+        
 
     def categorie_exist(self, choice_categorie):
         cursor = self.database.cursor()
@@ -50,7 +60,7 @@ class Input:
         rows = cursor.fetchall()
         if not rows:
             # Si pas de categorie trouvée, on affiche le menu de base
-            self.categorie_input()
+            self.categorie_menu()
         else:
             # Si une catégorie est trouvée, on affiche le menu des produits avec comme
             # paramètre le choix de la catégorie
@@ -62,21 +72,32 @@ class Input:
         cursor.execute(sql)
         produits = cursor.fetchall()
 
+        print("\n")
+        print(Color.OKGREEN + "Le programme a retourné {} produits".format(cursor.rowcount)+ Color.ENDC)
+        print("\n")
         self.produit_print(produits)
         self.produit_input(choice_categorie)
 
     def produit_print(self, produits):
         for produit in produits:
-            print ("{} -- {} -- {} ".format(produit.id, produit.name, produit.nutriscore))
+            print (Color.WARNING + "{} -- {} -- {} ".format(produit.id, produit.name, produit.nutriscore) + Color.ENDC)
+        print("\n")
 
     def produit_item_print(self, produit):
-            print ("Un substitu de trouver: ")
-            print ("{} -- {} -- {} ".format(produit.id, produit.name, produit.nutriscore))
+            print("\n")
+            print (Color.OKGREEN + "Le programme a retourné 1 substitu: "+ Color.ENDC)
+            print("\n")
+            print (Color.WARNING + "{} -- {} -- {} ".format(produit.id, produit.name, produit.nutriscore)+ Color.ENDC)
+            print("\n")
 
     def produit_input(self, choice_categorie):
-        choice_produit = input("Selectionner l'id d'un produit : ")
-        choice_produit = int(choice_produit)
-        self.produit_exist(choice_produit, choice_categorie)
+        choice_produit = input(Color.HEADER + "Selectionner l'id d'un produit : " + Color.ENDC)
+        try:
+            choice_produit = int(choice_produit)
+            self.produit_exist(choice_produit, choice_categorie)        
+        except ValueError:
+            self.produit_menu(choice_categorie)        
+
 
     def produit_exist(self, choice_produit, choice_categorie):
         cursor = self.database.cursor()
@@ -88,15 +109,18 @@ class Input:
         rows = cursor.fetchall()
         if not rows:
             # Si pas de produit trouvé, on affiche le menu des produits
+            print("\n")
+            print(Color.OKGREEN + "Le produit saisi n'appartient pas à la catégorie saisie" + Color.ENDC)
             self.produit_menu(choice_categorie)
         else:
             # Si une produit est trouvé, on affiche le menu du substitu
             self.produit_substitu_search(choice_produit)
 
     def produit_substitu_search(self, choice_produit):
+
         produit_item = self.produit_search(choice_produit)   
-        nutriscore_better = self.letter_before(produit_item.nutriscore)
-        produit_substitu = self.substitu_search(choice_produit, nutriscore_better)
+        nutriscore_liste = self.letter_before(produit_item.nutriscore)
+        produit_substitu = self.substitu_search(choice_produit, nutriscore_liste)
         if produit_substitu is not None:
             self.produit_item_print(produit_substitu)
             self.produit_substitu_input(produit_item, produit_substitu)
@@ -114,19 +138,54 @@ class Input:
         return categorie
 
     def produit_substitu_input(self, produit_item, produit_substitu):
-        choice_save = input("Voulez vous enregistrer en base de données le substitu? (oui/non) ")
+        choice_save = input(Color.HEADER + "Voulez vous enregistrer en base de données le substitu? (oui/non) " + Color.ENDC)
         if choice_save == 'oui':
             isInsert = self.produit_substitu_save(produit_item, produit_substitu)
             if isInsert == True :
-                print("Favoris enregistré")
+                print("\n")
+                print (Color.OKGREEN + "Le programme a enregistré le substitu du produit {} ".format(produit_item.name)+ Color.ENDC)
             self.display_menu()
             
-        else: 
+        elif choice_save == 'non': 
             self.display_menu()
+        else :
+            self.produit_substitu_input(produit_item, produit_substitu)
+
+    def produit_substitu_exist(self, produit_item):
+        cursor = self.database.cursor(named_tuple=True, buffered=True)
+        sql = "SELECT * FROM favoris WHERE produit_id = '{}' ".format(produit_item.id)
+        cursor.execute(sql)
+        rows = cursor.fetchone()
+        if not rows :
+            return False
+        else:
+            return True
 
     def produit_substitu_save(self, produit_item, produit_substitu):
         cursor = self.database.cursor()
+        is_produit_substitu_exist = self.produit_substitu_exist(produit_item)
+        if is_produit_substitu_exist == True :
+            isAction = self.produit_substitu_update(produit_item, produit_substitu)
+        else :
+            isAction = self.produit_substitu_insert(produit_item, produit_substitu)
+
+        return isAction 
+
+    def produit_substitu_insert(self, produit_item, produit_substitu):
+        cursor = self.database.cursor()
         sql = "INSERT INTO favoris (produit_id, produit_substitu_id) VALUES (%s,%s)"
+        product_id = produit_item.id
+        product_substitu_id = produit_substitu.id        
+        cursor.execute(sql, (product_id, product_substitu_id,) ) # sans la virgule, il y a un bug.
+        self.database.commit() 
+        if cursor.rowcount is not None:
+            return True
+        else:
+            return False
+
+    def produit_substitu_update(self, produit_item, produit_substitu):
+        cursor = self.database.cursor()
+        sql = " UPDATE favoris SET produit_substitu_id = %s WHERE produit_id = %s "
         product_id = produit_item.id
         product_substitu_id = produit_substitu.id        
         cursor.execute(sql, (product_id, product_substitu_id,) ) # sans la virgule, il y a un bug.
@@ -143,13 +202,19 @@ class Input:
         produit = cursor.fetchone()
         return produit
 
-    def substitu_search(self, choice_produit, nutriscore_better):
+    def substitu_search(self, choice_produit, nutriscore_liste):
         # Recherche de la catégorie du produit
         categorie = self.categorie_find(choice_produit)
 
         # Recherche d'un substitu
         cursor = self.database.cursor(named_tuple=True, buffered=True)
-        sql = "SELECT produits.* FROM produits INNER JOIN asso_produit_categorie ON asso_produit_categorie.produit_id = produits.id INNER JOIN categories ON categories.id = asso_produit_categorie.categorie_id WHERE categories.id =  '{}' AND produits.nutriscore = '{}' ".format(categorie.id, nutriscore_better)
+        nutriscore_in = "("
+        for item in nutriscore_liste:
+            nutriscore_in += " '"+item+"', "
+        nutriscore_in = nutriscore_in[:-2]
+        nutriscore_in += ")"
+
+        sql = "SELECT produits.* FROM produits INNER JOIN asso_produit_categorie ON asso_produit_categorie.produit_id = produits.id INNER JOIN categories ON categories.id = asso_produit_categorie.categorie_id WHERE categories.id =  '{}' AND produits.nutriscore IN {} ORDER BY produits.nutriscore ASC LIMIT 1 ".format(categorie.id, nutriscore_in)
         cursor.execute(sql)
         produit_substitu = cursor.fetchone()
         return produit_substitu
@@ -157,9 +222,18 @@ class Input:
     def letter_before(self, nutriscore):
         alphabetic = "abcdefghijklmnopqrstuvwxyz"
         position = alphabetic.find(nutriscore)
-        if position > -1:
-            nutriscore_better = alphabetic[position-1]        
+        if position != 1:
+            nutriscore_better = alphabetic[position]        
         else:
             nutriscore_better = alphabetic[0]
-        return nutriscore_better
+
+        liste_literal = []
+        if nutriscore_better == 'a' :
+            liste_literal.append(nutriscore_better)
+        else :
+            nutriscore_liste = alphabetic.split(nutriscore_better)[0]
+            for item in nutriscore_liste:
+                liste_literal.append(item)
+
+        return liste_literal
         
