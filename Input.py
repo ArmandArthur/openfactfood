25fedb717bdc7324b0f2deaa0478911d147bd4f5
ARMANDArthur <armand.arthur@gmail.com>
2019-11-02 13:04:03 +0100

    commentaires english on input.py

--- Input.py
+++ Input.py
@@ -9,12 +9,20 @@ from RequestSubstitu import RequestSubstitu
 
 class Input:
 
+    """
+        Récupère les "modeles" requetes
+        [EN] - GET request model
+    """
     def __init__(self):
         self.requestCategoryInstance = RequestCategory()
         self.requestProductInstance = RequestProduct()
         self.requestSubstituInstance = RequestSubstitu()
         self.display_menu()
 
+    """
+        Selection du type de menu, si choix non conforme, rappel de la fonction
+        [EN] - Choice of the menu, if choice fails, call the function itself
+    """
     def display_menu(self):
         print("\n")
         print(Color.WARNING + " 1 - Quel aliment souhaitez-vous remplacer ? " + Color.ENDC)
@@ -33,6 +41,12 @@ class Input:
         elif choice_menu == 2 :
             self.substitu_menu()
 
+    """
+        Liste la liste des catégories
+        Affiche le nombre de résultats de la requetes.
+        [EN] - List the categories
+        [EN] - Display the count of categories
+    """
     def categorie_menu(self):
         # pour avoir les attributs au lieu des tuples
         parameters =  self.requestCategoryInstance.liste()
@@ -42,11 +56,26 @@ class Input:
         self.categorie_print(parameters['categories'])
         self.categorie_input()
 
+    """
+        Affiche les catégories
+        [EN] - Display categories
+    """
     def categorie_print(self, categories):
         for categorie in categories:
             print (Color.WARNING + "{} -- {}".format(categorie.id, categorie.name)+ Color.ENDC)
         print("\n")
     
+    """
+        Propose à l'utilisateur de sélectionner l'id du catégorie
+        Si ID non conforme, rappel de la function categorie_menu
+        Idem, si ID conforrme mais pas de résultats sur la catégorie.
+        Sinon, affichage de la liste des produits avec le choix de la catégorie
+        
+        [EN] - User's choice to choose ID's category
+        [EN] - If ID not integer, call categorie_menu function
+        [EN] - If ID is integer but no categorie found, call categorie_menu function
+        [EN] - Else, display list of product with the choice_categorie
+    """
     def categorie_input(self):
         choice_categorie = input(Color.HEADER + "Selectionner l'id d'une categorie : "+ Color.ENDC)
         try:
@@ -55,7 +84,11 @@ class Input:
         except ValueError:
             self.categorie_menu()        
         
-
+    """
+        Expliqué dans le docstring de la fonction categorie_input
+        
+        [EN] - Explain in the doctring of the function categorie_input
+    """
     def categorie_exist(self, choice_categorie):
         parameters = self.requestCategoryInstance.exist(choice_categorie)
         if not parameters['rows']:
@@ -65,7 +98,14 @@ class Input:
             # Si une catégorie est trouvée, on affiche le menu des produits avec comme
             # paramètre le choix de la catégorie
             self.produit_menu(choice_categorie)
-
+    """
+        Le programme écrit le nombre de produits retournés par la requetes
+        Appel de la fonction produit_print et produit_input
+        
+        [EN] - Computer write the count of products found.
+        [EN] - Call function produit_print and produit_input
+        
+    """
     def produit_menu(self, choice_categorie):
         parameters = self.requestProductInstance.liste_from_category(choice_categorie)
         print("\n")
@@ -74,6 +114,12 @@ class Input:
         self.produit_print(parameters['produits'])
         self.produit_input(choice_categorie)
 
+    """
+        Affichage de la liste des produits avec espacements réguliers
+        
+        [EN] - Display the list of product's attributes with regular alignment.
+        
+    """
     def produit_print(self, produits):
         for produit in produits:
             print (Color.WARNING + "{:<3} -- {:<70} -- {:<2} ".format(produit.id, produit.name, produit.nutriscore) + Color.ENDC)
@@ -86,6 +132,11 @@ class Input:
             print (Color.WARNING + "{} -- {} -- {} ".format(produit.id, produit.name, produit.nutriscore)+ Color.ENDC)
             print("\n")
 
+    """
+        Sélection de l'ID du produit, vérification si le produit existe
+        
+        [EN] - Chooce of ID's product, verify si product exist
+    """
     def produit_input(self, choice_categorie):
         choice_produit = input(Color.HEADER + "Selectionner l'id d'un produit : " + Color.ENDC)
         try:
@@ -94,7 +145,15 @@ class Input:
         except ValueError:
             self.produit_menu(choice_categorie)        
 
+    """
+        Vérification si le produit existe dans la catégorie
+        Si pas résultats, le produit n'appartient pas à la catégorie, retour à la liste des produits de la catégorie
+        Sinon recherche d'un substitu
 
+        [EN] - Verify in the product exist in the category
+        [EN] - If no results, product don't behove of the category, back to the list of product of category
+        [EN] - Else search a subsitute
+    """
     def produit_exist(self, choice_produit, choice_categorie):
         parameters = self.requestProductInstance.exist(choice_produit, choice_categorie)
         if not parameters['rows']:
@@ -106,6 +165,22 @@ class Input:
             # Si une produit est trouvé, on affiche le menu du substitu
             self.produit_substitu_search(choice_produit)
 
+    """
+        produit_find retourne un produit correspondant à l'ID du produit
+        letter_before retourne un tableau avec les nutriscores supérieurs à celui du produit
+        substitu_find retourne un produit de subsitu trouvé
+        Si résultats, affichage du produit substitu trouvé, et appel à la proposition de sauvegarde.
+        Sinon, on recherche la catégorie du produit sélectionné pour afficher la liste des produits correspondants
+        à cette catégorie.
+
+        [EN] - produit_find return a product corresponding to the ID product
+        [EN] - letter_before return an array with nutriscores better than nutriscore of product choosen.
+        [EN] - substitu_find return a product substitute
+        [EN] - If results, display product substitute found, and call question of save.
+        [EN] - Else, find the category of product choose for for displaying the liste of products corresponding 
+        to this category.
+
+    """
     def produit_substitu_search(self, choice_produit):
 
         produit_item = self.produit_find(choice_produit)   
@@ -126,6 +201,19 @@ class Input:
         parameters = self.requestProductInstance.category_find(choice_produit)
         return parameters
 
+    """ 
+        Voulez vous sauvegarder le substitu en base?
+        produit_substitu_save fait un insert ou un update.
+        Si enregistré, message de confirmation + appel du menu principal
+        Si choix non, appel du menu principal
+        Sinon, si l'utilisateur n'a pas choici oui ou non, on rappel la fonction
+    
+        [EN] - Do you want save the subsitute in database?
+        [EN] - If saved, confirmation message and call menu first
+        [EN] - If chooce no, call menu first
+        [EN] - Else, recall the same function
+    """
+
     def produit_substitu_input(self, produit_item, produit_substitu):
         choice_save = input(Color.HEADER + "Voulez vous enregistrer en base de données le substitu? (oui/non) " + Color.ENDC)
         if choice_save == 'oui':
@@ -140,10 +228,20 @@ class Input:
         else :
             self.produit_substitu_input(produit_item, produit_substitu)
 
+    """ 
+        Retourne true or false si le produit subsitu existe déjà
+
+        [EN] - Return true or false if the product exist or not.
+    """
     def produit_substitu_exist(self, produit_item):
         isExist = self.requestSubstituInstance.exist(produit_item)
         return isExist
 
+    """ 
+        Retourne true si le produit est insérer/updater (pour le message)
+        [EN] - Return true if product subsitute is insert/update (for the message).
+
+    """
     def produit_substitu_save(self, produit_item, produit_substitu):
         is_produit_substitu_exist = self.produit_substitu_exist(produit_item)
         if is_produit_substitu_exist == True :
@@ -167,10 +265,22 @@ class Input:
         else:
             return False
 
+    """ 
+        Recherche un produit par son ID - appelé par produit_substitu_search
+        [EN] - Search a product by a ID - call by produit_substitu_search
+
+    """
     def produit_find(self, choice_produit):
         parameters = self.requestProductInstance.find(choice_produit)
         return parameters['produit']
 
+    """ 
+        Recherche un produit substitu par le nutriscore et la catégorie du produit
+         - appelé par produit_substitu_search
+        [EN] - Search a product substitute by nutriscore and product's category
+         - call by produit_substitu_search
+
+    """
     def substitu_find(self, choice_produit, nutriscore_liste):
         # Recherche de la catégorie du produit
         parameters = self.category_find(choice_produit)
@@ -186,7 +296,13 @@ class Input:
         params = self.requestSubstituInstance.find(parameters['categorie'], nutriscore_in)
         return params['product_substitu']
 
+    """ 
+        Construit le WHERE_IN de la requete
+         - appelé par produit_substitu_search
+        [EN] - Build WHERE_IN of request
+         - call by produit_substitu_search
 
+    """
     def letter_before(self, nutriscore):
         alphabetic = "abcdefghijklmnopqrstuvwxyz"
         position = alphabetic.find(nutriscore)
@@ -204,7 +320,15 @@ class Input:
                 liste_literal.append(item)
 
         return liste_literal
+
+    """ 
+        Liste des substituts trouvés
+        Appelle la fonction print et l'input
+
+        [EN] - List of substitutes found
+        [EN] - Call function print and input
         
+    """    
     def substitu_menu(self):
         parameters = self.requestSubstituInstance.liste()
         print("\n")
@@ -213,6 +337,12 @@ class Input:
         self.produit_print(parameters['products'])
         self.substitu_input()
 
+    """ 
+        Choix du produit sur lequel on veut avoir le substitut
+
+        [EN] - Choose product where we want see the substitute
+        
+    """    
     def substitu_input(self):
         choice_product = input(Color.HEADER + "Selectionner l'id d'un produit : "+ Color.ENDC)
         try:
@@ -225,11 +355,23 @@ class Input:
         except ValueError:
             self.substitu_menu()   
 
+    """ 
+        Vérifie si un substitu existe
+
+        [EN] - Vérify if a substitue exist
+        
+    """    
     def substitu_exist(self, choice_product):
         parameters = self.requestProductInstance.find(choice_product)
         isExist = self.requestSubstituInstance.exist(parameters['produit'])
         return isExist
 
+    """ 
+        Affiche le subsitue puis le menu principal
+
+        [EN] - Display the substitute and call the main menu
+        
+    """    
     def substitu_item(self, choice_product):
         parameters = self.requestSubstituInstance.item_from_product(choice_product)
         self.produit_item_print(parameters['product'])
